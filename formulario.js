0c3d9e95872d8c7416490c2aac316a1347ebf11d
Lenny896 <168930543+Lenny896@users.noreply.github.com>
2024-05-26 18:50:32 -0300

    Detalles

--- formulario.js
+++ formulario.js
@@ -2,30 +2,30 @@ function validarEnviar(){
     parrafo = document.getElementById("error")
     if(document.formulario.nombre.value.length <= 2){
         document.formulario.nombre.focus()
-        parrafo.innerHTML = "Nombre incorrecto."
+        parrafo.innerHTML = "Escriba su nombre completo."
         return
     }
     let ExpReg =  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
     let validez = ExpReg.test(document.formulario.correo.value);
     if(validez == false){
         document.formulario.correo.focus()
-        parrafo.innerHTML = "Correo incorrecto."
+        parrafo.innerHTML = "Correo electrónico incorrecto."
         return
     }
     let NumeroEntero = parseInt(document.formulario.celular.value)
     if(isNaN(NumeroEntero)){
         document.formulario.celular.focus()
-        parrafo.innerHTML = "Número incorrecto."
+        parrafo.innerHTML = "Número de telefono incorrecto."
         return
     }
     if(document.formulario.interes.selectedIndex == 0){
         document.formulario.interes.focus()
-        parrafo.innerHTML = "Elija una opción."
+        parrafo.innerHTML = "Elija el mobiliario de interés."
         return 
     }
     if(document.formulario.motivo.selectedIndex == 0){
         document.formulario.motivo.focus()
-        parrafo.innerHTML = "Elija una opción."
+        parrafo.innerHTML = "Elija el motivo de su consulta."
         return 
     }
     parrafo.innerHTML = "Gracias por completar el formulario. A la brevedad nos pondremos en contacto con usted."
